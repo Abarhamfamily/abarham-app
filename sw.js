418a5fb86fbd35a91a38c0e266f9ef86f4517b87
abarhamfamilay <najafimonfared84@gmail.com>
2026-08-09 18:47:55 +0330

    Fix PWA fetch handler in sw.js and manifest setup

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
-            .then(() => self.clients.claim())
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
     );
 });
-self.addEventListener('fetch', () => {});
